180d28fbf0143e41a9d7034fa765e6fe975b9fc3
Oihan75 <98814367+Oihan75@users.noreply.github.com>
2022-03-24 14:58:12 +0100

    Add files via upload

--- Ghibli/ghibli.js
+++ Ghibli/ghibli.js
@@ -8,5 +8,22 @@ function addElement() {
     document.getElementById("div").appendChild(newDiv);
     }
 }
+document.getElementById("date").innerHTML= "Trouver un film";
+let btn = document.querySelector('input');
+btn.addEventListener('click', enterDate);
+
+function enterDate(){
+   let entree= prompt("Entrez une date entre 1985 et 2020", "<Entrer votre date>");
+    if (entree==null){
+        alert("vous avez annule");
+    }
+        if (entree<1985){
+            document.getElementById("date").innerHTML= "Le studio n'existait pas encore. Il fut fondé en 1985.";
+        } else if (entree>2020){
+            document.getElementById("date").innerHTML= "Le studio n'a pas fait de films après 2020";
+        } else{
+            document.getElementById("date").innerHTML= "Afficher";
+        }
+}
 
 addElement()
