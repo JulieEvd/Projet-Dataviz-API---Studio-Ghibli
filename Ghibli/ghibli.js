1d129b82bcd616cc51dc3a13e193b0b60d676062
clotildebeckerlamoureux <clobecker77@gmail.com>
2022-03-29 11:24:46 +0200

    Add loops to display elements by date

--- Ghibli/ghibli.js
+++ Ghibli/ghibli.js
@@ -23,42 +23,83 @@
 // ]
 
 var films = JSON.parse($.ajax({'url': "https://ghibliapi.herokuapp.com/films/", 'async': false}).responseText)
+let entree
 
 function addElement() {
+  btn.addEventListener('click', enterDate)
+  console.log(entree)
+  if (entree!=null) {
     for (i=0; i < films.length; i++) {
-    const newDiv = document.createElement("div")
-    const poster  = document.createElement("section")
-    poster.className = "poster"
-    const resume = document.createElement("section")
-    resume.className = "resume"
-    poster.innerHTML = '<img id="image" height="200" width="140" src="' + films[i]["image"] + '" />'
-    resume.innerHTML =  films[i]["title"] + "<br />" + "<br />" + "Année de sortie: " + films[i]["release_date"] +"<br />" + "Durée en minutes: "+ films[i]["running_time"] + "<br />" + films[i]["description"]
-    document.getElementById("div").appendChild(newDiv)
-    newDiv.prepend(poster)
-    newDiv.append(resume)
+      if (films[i]["release_date"]==entree) {
+        for (i=0; i < films.length; i++) {
+          var element = document.getElementById("div")
+            while (element.firstChild) {
+                element.removeChild(element.firstChild)
+             }
+          const newDiv = document.createElement("div")
+          const poster  = document.createElement("section")
+          poster.className = "poster"
+          const resume = document.createElement("section")
+          resume.className = "resume"
+          poster.innerHTML = '<img id="image" height="200" width="140" src="' + films[i]["image"] + '" />'
+          resume.innerHTML =  films[i]["title"] + "<br />" + films[i]["original_title"] + "&nbsp;" + "&nbsp;"+ films[i]["original_title_romanised"] +"<br />" + "<br />" + "Year of release: " + films[i]["release_date"] +"<br />" + "Duration (min): "+ films[i]["running_time"] + "<br />" + "<br />" + films[i]["description"]
+          document.getElementById("div").appendChild(newDiv)
+          newDiv.prepend(poster)
+          newDiv.append(resume)
+          }
+      }
     }
+  } else {
+        //console.log("film")
+        // const newDiv = document.createElement("div")
+        // newDiv.innerHTML = films ["title"]
+        // document.getElementById("div").appendChild(newDiv)
+        //  for (i=0; i < films.length; i++) {
+        //    var element = document.getElementById("div")
+        //      while (element.lastChild) {
+        //          element.removeChild(element.lastChild)
+        //      }
+      for (i=0; i < films.length; i++) {
+        const newDiv = document.createElement("div")
+        const poster  = document.createElement("section")
+        poster.className = "poster"
+        const resume = document.createElement("section")
+        resume.className = "resume"
+        poster.innerHTML = '<img id="image" height="200" width="140" src="' + films[i]["image"] + '" />'
+        resume.innerHTML =  films[i]["title"] + "<br />" + films[i]["original_title"] + "&nbsp;" + "&nbsp;"+ films[i]["original_title_romanised"] +"<br />" + "<br />" + "Year of release: " + films[i]["release_date"] +"<br />" + "Duration (min): "+ films[i]["running_time"] + "<br />" + "<br />" + films[i]["description"]
+        document.getElementById("div").appendChild(newDiv)
+        newDiv.prepend(poster)
+        newDiv.append(resume)
+      
+    }
+  }
 }
 
 
-document.getElementById("date").innerHTML= "Trouver un film";
-let btn = document.querySelector('input');
-btn.addEventListener('click', enterDate);
+document.getElementById("date").innerHTML= "Trouver un film"
+let btn = document.querySelector('input')
+//btn.addEventListener('click', enterDate)
 
 function enterDate(){
-   let entree= prompt("Entrez une date entre 1985 et 2021", "<Entrer votre date>");
+   entree= prompt("Entrez une date entre 1985 et 2021", "<Entrer votre date>")
     if (entree==null){
-        alert("vous avez annule");
+        alert("vous avez annule")
     }
         if (entree<1985){
-            document.getElementById("date").innerHTML= "Le studio n'existait pas encore. Il fut fondé en 1985.";
+            document.getElementById("date").innerHTML= "Le studio n'existait pas encore. Il fut fondé en 1985."
         } else if (entree>2021){
-            document.getElementById("date").innerHTML= "Le studio n'a pas fait de films après 2021";
+            document.getElementById("date").innerHTML= "Le studio n'a pas fait de films après 2021"
         } else{
-            document.getElementById("date").innerHTML= "Afficher";
+          return addElement()
+            //document.getElementById("date").innerHTML= "Afficher"
         }
 }
 
+
+
 addElement()
+
+
 
  
 //document.getElementById("example").innerHTML = json[0]["release_date"]
